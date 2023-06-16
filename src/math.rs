fd8fbb5c998f609fb1bcdf694ee14d76e53d4e2d
Eddie Cheng <schaffenneu47@gmail.com>
2023-06-16 17:38:38 +0800

    add calc_factorial

--- src/math.rs
+++ src/math.rs
@@ -1,2 +1,21 @@
+pub fn calc_factorial(num: u64) -> u64 {
+    let mut result = 1;
 
+    for i in 1..=num {
+        result *= i;
+    }
 
+    result
+}
+
+#[cfg(test)]
+mod tests {
+     use super::*;
+
+    #[test]
+    fn test_calc_factorial() {
+      let fac = calc_factorial(3);
+
+      assert_eq!(fac, 6);
+    }
+}
