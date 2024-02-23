903704d4ab0a290b8444f052d620b5b7a195503f
R4 Cheng <karaburi2023@gmail.com>
2024-02-23 20:04:29 +0800

    styling

--- src/calc.rs
+++ src/calc.rs
@@ -9,44 +9,44 @@ pub fn calc_factorial(num: u64) -> u64 {
 }
 
 pub fn calc_combination(n: u64, k: u64) -> u64 {
-  let mut numerator = 1;
-  let mut denominator = 1;
+    let mut numerator = 1;
+    let mut denominator = 1;
 
-  // loop from n to k
-  for i in (n-k+1)..=n {
-    numerator *= i;
-  }
+    // loop from n to k
+    for i in (n - k + 1)..=n {
+        numerator *= i;
+    }
 
-  // loop from 1 to k
-  for i in 1..=k {
-    denominator *= i;
-  }
+    // loop from 1 to k
+    for i in 1..=k {
+        denominator *= i;
+    }
 
-  numerator / denominator
+    numerator / denominator
 }
 
 #[cfg(test)]
 mod tests {
-     use super::*;
+    use super::*;
 
     #[test]
     fn calc_factorial_of_five() {
-      let fac = calc_factorial(3);
+        let fac = calc_factorial(3);
 
-      assert_eq!(fac, 6);
+        assert_eq!(fac, 6);
     }
 
     #[test]
     fn c_five_chose_two() {
-      let comb = calc_combination(5, 2);
+        let comb = calc_combination(5, 2);
 
-      assert_eq!(comb, 10);
+        assert_eq!(comb, 10);
     }
 
     #[test]
     fn c_thiry_chose_two() {
-      let comb = calc_combination(30, 2);
+        let comb = calc_combination(30, 2);
 
-      assert_eq!(comb, 435);
+        assert_eq!(comb, 435);
     }
 }
